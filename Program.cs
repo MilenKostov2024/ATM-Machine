d2e95ed3b1e2ea6ecc621637f860a437404d1617
MilenKostov2024 <milen.kostov1717@gmail.com>
2025-11-15 13:56:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ATM Machine";
+
+            cardHolder currentUser = new cardHolder();
+
+            while (true)
+            {
+                Console.WriteLine("Please choose from one of the following options...");
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Show Balance");
+                Console.WriteLine("4. Exit");
+
+                Console.Write("Your choice: ");
+                int choose = Int32.Parse(Console.ReadLine());
+
+                switch (choose)
+                {
+                    case 1:
+                        Console.Write("How much money would you like to deposit: ");
+                        decimal deposit = Decimal.Parse(Console.ReadLine());
+                        currentUser.setBalance(currentUser.getBalance() + deposit);
+                        Console.WriteLine("Thank you for your deposit. Your new balance is: " + currentUser.getBalance() + " BGN");
+                        break;
+
+                    case 2:
+                        Console.Write("How much money would you like to withdraw: ");
+                        decimal withdraw = Decimal.Parse(Console.ReadLine());
+
+                        if (currentUser.getBalance() < withdraw)
+                        {
+                            Console.WriteLine("Insufficient balance :(");
+                        }
+                        else
+                        {
+                            currentUser.setBalance(currentUser.getBalance() - withdraw);
+                            Console.WriteLine("You have withdrawn: " + withdraw + " BGN. Your new balance is: " + currentUser.getBalance() + " BGN");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Your current balance is: " + currentUser.getBalance() + " BGN");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Thank you! Have a nice day!");
+                        return; 
+                }
+
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
---
+++ cardHolder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class cardHolder
+    {
+        // дефиниране на полета
+        string cardNum;
+        int pin;
+        string firstName;
+        string lastName;
+        decimal balance;
+
+        // конструктор с параметри
+        public cardHolder(string cardNum, int pin, string firstName, string lastName, decimal balance)
+        {
+            this.cardNum = cardNum;
+            this.pin = pin;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.balance = balance;
+        }
+
+        // конструктор без параметри
+        public cardHolder()
+        {
+            this.cardNum = null;
+            this.pin = 0;
+            this.firstName = null;
+            this.lastName = null;
+            this.balance = 0;
+        }
+
+        // Инициализиране на свойства
+        public string getNum() { return cardNum; }
+        public int getPin() { return pin; }
+        public string getFirstName() { return firstName; }
+        public string getLastName() { return lastName; }
+        public decimal getBalance() { return balance; }
+
+        // Инициализиране на методи
+        public void setNum(string newCardNum) { cardNum = newCardNum; }
+        public void setPin(int newPin) { pin = newPin; }
+        public void setFirstName(string newFirstName) { firstName = newFirstName; }
+        public void setLastName(string newLastName) { lastName = newLastName; }
+        public void setBalance(decimal newBalance) { balance = newBalance; } 
+
+    }
+}
